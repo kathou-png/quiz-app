e0447573b8cb3af5f2b24a7089374fd9acff741b
ialexvo <alex.vo@edu.esiee.fr>
2022-05-31 11:13:24 +0200

    Add initial front configurations

---
+++ quiz-ui/src/views/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Home page</h1>
+</template>
+
+<script>
+import quizApiService from "@/services/quizApiService";
+
+export default {
+  name: "HomePage",
+  data() {
+    return {
+    };
+  },
+  async created() {
+		console.log("Composant Home page 'created'");
+  }
+};
+</script>
