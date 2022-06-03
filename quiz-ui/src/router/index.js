cf58d6e47d64a8e852f1f05db7dc44886c6646d8
ialexvo <alex.vo@edu.esiee.fr>
2022-06-03 14:17:14 +0200

    add questions page

--- quiz-ui/src/router/index.js
+++ quiz-ui/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
+import NewQuizPage from '../views/NewQuizPage.vue'
+import QuestionsManager from '../views/QuestionsManager.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,10 +22,16 @@ const router = createRouter({
     {
       path: '/start-new-quiz-page',
       name: 'NewQuizPage',
+      component: NewQuizPage
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/NewQuizPage.vue')
+      //component: () => import('../views/NewQuizPage.vue')
+    },
+    {
+      path: '/questions',
+      name: 'Questions',
+      component: QuestionsManager
     }
   ]
 })
---
+++ quiz-ui/src/services/ParticipationStorageService.js
@@ -0,0 +1,17 @@
+export default {
+  clear() {
+    // todo : implement
+  },
+  savePlayerName(playerName) {
+    window.localStorage.setItem("playerName", playerName);
+  },
+  getPlayerName() {
+    return window.localStorage.getItem("playerName");
+  },
+  saveParticipationScore(participationScore) {
+    // todo : implement
+  },
+  getParticipationScore() {
+    // todo : implement
+  }
+};
